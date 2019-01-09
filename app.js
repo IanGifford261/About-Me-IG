aa227f47b4f008084378fb90e8679c02c9ab7596
iangifford <lightyagamie10@gmail.com>
2019-01-08 17:12:35 -0800

    finalized my index.html and app.js to meet lab criteria

--- app.js
+++ app.js
@@ -28,3 +28,23 @@ var age = prompt('Am I seventeen years old? Yes or No.').toUpperCase();
         if (age === ('NO') || age === ('N'))
         alert('Good guess. I am definitly not seventeen')
     }
+
+var water = prompt('Do you think I am allergic to water? Yes or No.').toUpperCase();
+    console.log (water);
+
+    if (water === ('YES') || water === ('Y')) {
+        alert('Nooooo, I dont think that I know of many people that are haha. That would suck')
+    } else {
+        if (water === ('NO') || water === ('N'))
+        alert('Correct! I am not. Imagine how awful would it be to live in a rainy place and be allergic to water!')
+    }
+
+var narnia = prompt('Am I from Narnia? Yes or No.').toUpperCase();
+    console.log (narnia);
+
+    if (narnia === ('YES') || narnia === ('Y')){
+        alert('YES! haha nahhhh im just kidding... Im not... would be cool though!')
+    } else {
+        if (narnia === ('NO') || narnia === ('N'))
+        alert('Yeah... sadface... Youre correct. Sadly')
+    }
