bb119f3738f9d857e38aa703f2cfc59c7f9e759b
iangifford <lightyagamie10@gmail.com>
2019-01-08 16:55:51 -0800

    Added even more css styling, rewrote my app.js to apply to the lab.

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var kids = prompt('how many kids do you have?');
-console.log('number of kids:', kids);
+var answercar = prompt('Do I like cars? Please answer Yes or No').toUpperCase();   
+    console.log (answercar);
+    
 
-var guitars = prompt('how many guitars to you have');
-console.log('number of guitars:', guitars);
+    if (answercar === ('YES') || answercar === ('Y')) {
+        alert('Yay! Thats right! Im learning to code to feed that passion');
+    } else {
+        if (answercar === ('NO') || answercar === ('N'))
+        alert('Whoops! Nope thats wrong. I absolutely LOVE cars')
+    }
 
-if (kids === '3' || kids === '4') {
-    alert('yeah, you are correct. too many kids');
-}
+var seattle = prompt('Am I a Seattle native? Yes or No.').toUpperCase();
+    console.log (seattle);
 
-if (parseInt(kids) === 3 && parseInt(guitars) === 3) {
-    alert('yeah you are right');
-} else {
-    alert('you are wrong');
-}
+    if (seattle === ('YES') || seattle === ('Y')) {
+        alert('Nope! unfortunately I am not. I wish I was')
+    } else {
+        if (seattle === ('NO') || seattle === ('N'))
+        alert('You are correct, I dreamed of living here for years and finally made it happen New Years of 2019')
+    }
 
+var age = prompt('Am I seventeen years old? Yes or No.').toUpperCase();
+    console.log (age);
+
+    if (age === ('YES') || age === ('Y')) {
+        alert('NO haha, I dont know many seventeen year olds with a beard')
+    } else {
+        if (age === ('NO') || age === ('N'))
+        alert('Good guess. I am definitly not seventeen')
+    }
