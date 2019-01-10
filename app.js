ad62e66660a7e82f8c977aca38e886a243abe2df
iangifford <lightyagamie10@gmail.com>
2019-01-09 19:11:58 -0800

    Finalized my last 2 question prompts and score counter, also made the css style more mellow, added 'top ten' Ordered list, and an experience Unordered list.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+var score = 0;
 
 var answercar = prompt('Do I like cars? Please answer Yes or No').toUpperCase();   
     console.log (answercar);
@@ -7,6 +7,7 @@ var answercar = prompt('Do I like cars? Please answer Yes or No').toUpperCase();
 
     if (answercar === ('YES') || answercar === ('Y')) {
         alert('Yay! Thats right! Im learning to code to feed that passion');
+        score++;
     } else {
         if (answercar === ('NO') || answercar === ('N'))
         alert('Whoops! Nope thats wrong. I absolutely LOVE cars')
@@ -20,6 +21,7 @@ var seattle = prompt('Am I a Seattle native? Yes or No.').toUpperCase();
     } else {
         if (seattle === ('NO') || seattle === ('N'))
         alert('You are correct, I dreamed of living here for years and finally made it happen New Years of 2019')
+        score++;
     }
 
 var age = prompt('Am I seventeen years old? Yes or No.').toUpperCase();
@@ -30,6 +32,7 @@ var age = prompt('Am I seventeen years old? Yes or No.').toUpperCase();
     } else {
         if (age === ('NO') || age === ('N'))
         alert('Good guess. I am definitly not seventeen')
+        score++;
     }
 
 var water = prompt('Do you think I am allergic to water? Yes or No.').toUpperCase();
@@ -40,6 +43,7 @@ var water = prompt('Do you think I am allergic to water? Yes or No.').toUpperCas
     } else {
         if (water === ('NO') || water === ('N'))
         alert('Correct! I am not. Imagine how awful would it be to live in a rainy place and be allergic to water!')
+        score++;
     }
 
 var narnia = prompt('Am I from Narnia? Yes or No.').toUpperCase();
@@ -50,6 +54,7 @@ var narnia = prompt('Am I from Narnia? Yes or No.').toUpperCase();
     } else {
         if (narnia === ('NO') || narnia === ('N'))
         alert('Yeah... sadface... Youre correct. Sadly')
+        score++;
     }
 
     
@@ -65,12 +70,41 @@ var narnia = prompt('Am I from Narnia? Yes or No.').toUpperCase();
         if(carsOwned === 7) {
             alert('Spot on!');
             i = 0;
-    } else 
-        if(carsOwned < 7) {
+            score++;
+    } else if(carsOwned < 7){
             alert('Too Low');
-    } else
-        if(carsOwned > 7) {
+    } else if(carsOwned > 7) {
             alert('Too High!')
     }
 }
 
+var carsFav = ['nissan', 'toyota', 'bmw']
+
+for (var x = 6; x > 0 ; x--) {
+   var brands = prompt('What are three of my favorite car brands? You have 6 attempts. A hint, I mentioned them before on my page').toLowerCase();
+   console.log('car brands', brands);
+
+   if (brands === carsFav[0]) {
+       alert('That is correct!') 
+       x=0;
+       score++;
+   }
+   if (brands === carsFav[1]) {
+       alert('That is correct!') 
+       x=0;
+       score++
+   }
+   if (brands === carsFav[2]) {
+       alert('That is correct!')
+       x=0;
+       score++;
+   }
+   else {
+       alert('Try Again.')
+   }
+    if (x < 1) {
+        alert('my top three brands are nissan, toyota, and bmw.')
+    }
+}
+
+    alert('Your score is ' + score + ' ,good job')
